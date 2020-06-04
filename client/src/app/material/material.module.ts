1402fe05602cbb547285a7932f32d7199e1d0a2c
Mauro <mauropomardurruthy@gmail.com>
2020-06-04 19:29:33 +0200

    Mi primer commit

--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-//import {MatButtonModule} from '@angular/material/button';
-//import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from "@angular/material/icon";
 
 
 const modules = [
     CommonModule,
-//    MatButtonModule,
-//    MatIconModule
+    MatButtonModule,
+    MatIconModule
 ];
 
 @NgModule({
