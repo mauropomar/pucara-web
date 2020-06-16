ff05c34661e7521ed78a514c2bd73d3eda2fcfe3
Mauro <mauropomardurruthy@gmail.com>
2020-06-16 20:48:22 +0200

    mi primer

---
+++ api/routes/customer.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+var CustomerController = require('../controllers/category_customer');
+var multipart = require('connect-multiparty');
+var md_auth = require('../middlewares/authenticated');
+
+var api = express.Router();
+api.post('/register-customer', md_auth.ensureAuth, CustomerController.saveCustomer);
+api.get('/customers/:page?',md_auth.ensureAuth, CustomerController.getCustomers);
+api.get('/customer/:id', md_auth.ensureAuth, CustomerController.getCustomer);
+api.put('/update-customer/:id', md_auth.ensureAuth, CustomerController.updateCustomer);
+api.delete('/delete-customer/:id', md_auth.ensureAuth, CustomerController.deleteCustomer);
+
+module.exports = api;
