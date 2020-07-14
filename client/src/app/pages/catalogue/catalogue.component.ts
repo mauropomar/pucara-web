9ddf2543a46402d179a1995e0c365108e63b9ab7
Mauro <mauropomardurruthy@gmail.com>
2020-07-14 22:16:38 +0200

    my first

--- client/src/app/pages/catalogue/catalogue.component.ts
+++ client/src/app/pages/catalogue/catalogue.component.ts
@@ -3,6 +3,8 @@ import { CatalogueService } from '../../services/catalogue.service';
 import { GlobalService } from "../../services/global.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { UtilService } from "../../services/util.service";
+import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-catalogue',
@@ -18,7 +20,12 @@ export class CatalogueComponent implements OnInit {
   public page:number = 1;
   public total;
 
-  constructor(private service: CatalogueService, private global: GlobalService,
+  allpost;
+  notEmptyPost = true;
+  notscrolly = true;
+
+  constructor(private http: HttpClient, private spinner:NgxSpinnerService,
+    private service: CatalogueService, private global: GlobalService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private util: UtilService) {
@@ -36,11 +43,37 @@ export class CatalogueComponent implements OnInit {
         this.total = data['total'];
         this.pages = data['pages'];
       })
+     /* const url = 'http://tlino.96.lt/api/getblogpost';
+      this.http.get(url).subscribe(data=>{
+           this.allpost = data[0];
+      });*/
   }
 
   onScroll(){
-    debugger
-    console.log('scrolled');
+    console.log('ss')
+    if(this.notscrolly && this.notEmptyPost){
+      this.spinner.show();
+      this.notscrolly = false;
+      this.loadNextPost()
+    }
+  }
+
+  loadNextPost(){
+    const url = 'http://tlino.96.lt/api/getnextpost';
+    const lastpost = this.allpost[this.allpost.length -1];
+    const lastpostId = lastpost.id;
+    const dataSend = new FormData();
+    dataSend.append('id', lastpostId);
+    this.http.post(url, dataSend)
+       .subscribe((data:any) =>{
+           const newPost = data[0];
+           this.spinner.hide();
+           if(newPost.length == 0){
+             this.notEmptyPost = false;
+           }
+           this.allpost = this.allpost.concat(newPost);
+           this.notscrolly = true;
+       })
   }
 
 }
